d3791c72ecaf9dcccda4e634427baeaedc03351b
vincentsheehan <vin.twentywo@gmail.com>
2020-12-05 15:04:51 +0000

    Add plot.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # Get input from user, and return as data frame.
@@ -31,11 +32,31 @@ def run_menu():
 
     return option
 
+def menu_option_1():
+    df_entry = get_entry()
+    add_to_df(df_log, df_entry)
+
+
+#def menu_option_2():
+
+
+#def menu_option_3():
+
+
+#def menu_option_4():
+
+
 
 if __name__ == '__main__':
     # To do: load CSV as data frame. If not present create empty data frame
-    run_menu()
-    df_log = pd.DataFrame([], columns=["Date", "Reading"])
-    df_entry = get_entry()
-    print(add_to_df(df_log, df_entry))
+    #run_menu()
+    #df_log = pd.DataFrame([], columns=["Date", "Reading"])
+    #df_entry = get_entry()
+    #print(add_to_df(df_log, df_entry))
     # Write data frame to CSV - to persist over separate runs.
+
+    df_readings = pd.read_csv("Meter Readings.csv")
+
+    plt.plot(df_readings["Date"], df_readings["Reading"])
+    plt.show()
+
